b75b07f3e4263e8c49d12d5fa7330bf3dca5d60c
melekhomrani <melekhomrani87@gmail.com>
2023-05-06 10:10:39 +0100

    index

---
+++ src/pages/Index.tsx
@@ -0,0 +1,37 @@
+import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+function Home() {
+  //TODO: get user role from context
+  const adminRole = "admin"
+  const userRole = "user"
+
+  return (
+    <>
+      <Header />
+      <Flex align="center" justify="center" minH="77vh">
+        <Box maxW="container.md" mx="auto" px="6">
+          <Heading as="h1" size="3xl" mb="6" textAlign="center" color="#ed1c24">
+            Welcome to Esprit Reclamation Management System
+          </Heading>
+          <Text fontSize="xl" mb="12" textAlign="center" color="black">
+            Our Reclamation Management System helps you streamline the complaint management process and improve customer satisfaction. With our easy-to-use platform, you can manage customer complaints or requests for compensation, assign them to the appropriate teams or individuals, and track their progress.
+          </Text>
+          <Flex justify="center">
+            <Link to={userRole ? "/user" : "/admin"}>
+              <Button colorScheme={"red"} color="white" size="lg" px="8">
+                Get Started
+              </Button>
+            </Link>
+          </Flex>
+        </Box>
+      </Flex>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
