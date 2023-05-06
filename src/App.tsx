2e1cba17d1c81131ed75ee165454e2b667f72ede
melekhomrani <melekhomrani87@gmail.com>
2023-05-06 09:46:01 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { ChakraProvider } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import Router from './Router'
+
+function App() {
+    const queryClient = new QueryClient()
+
+    return (
+        <>
+            <QueryClientProvider client={queryClient}>
+                <ChakraProvider>
+                    <Router />
+                </ChakraProvider>
+                <ReactQueryDevtools
+                    initialIsOpen={false}
+                    position='bottom-right'
+                />
+            </QueryClientProvider>
+        </>
+    )
+}
+
+export default App
