efe1690c358b1df1e79196e5b4432040e5177071
melekhomrani <melekhomrani87@gmail.com>
2023-05-21 22:50:42 +0100

    updated

--- src/hooks/useCreateComment.tsx
+++ src/hooks/useCreateComment.tsx
@@ -7,8 +7,12 @@ export interface Comment {
 }
 
 const createComment = async (comment: Comment) => {
+    console.log(comment.text)
     const res = await axios.post(`/api/gest/comments/${comment.reclamationId}`, comment.text, {
-        headers: { Authorization: 'Bearer ' + localStorage.getItem('token') },
+        headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('token'),
+            'Content-Type': 'application/json'
+        },
     })
     return res.data
 }
