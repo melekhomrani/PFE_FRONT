6f1dbbac6cdb861c22afe6ccde4f97d0e4d8ddbc
yassine <sassou.est100@gmail.com>
2023-05-21 17:19:20 +0100

    added breakpoints for diff sizes

--- src/pages/Admin/Calendar.tsx
+++ src/pages/Admin/Calendar.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
 import { ResponsiveCalendar } from '@nivo/calendar'
-import { Box, FormLabel, HStack, Input } from '@chakra-ui/react'
+import { Box, FormLabel, HStack, Input, Text, useBreakpointValue } from '@chakra-ui/react'
 import useGetCalendarData from '../../hooks/useGetCalendarData';
 import { useQueryClient } from '@tanstack/react-query';
 
 function Calendar() {
-  var dateOffset = (24*60*60*1000)
+  var dateOffset = (24*60*60*1000);
+  const direction = useBreakpointValue({ base:"vertical", lg:"horizontal", xl:"horizontal", "2xl":"horizontal", md:"vertical", sm:"vertical"})
   const today = new Date();
   const yesterday = new Date(today.getTime() - dateOffset);
-  const [from, setFrom] = useState<string>(yesterday.toISOString().slice(0, 10));
+  const [from, setFrom] = useState<string>("2023-04-21");
   const [To, setTo] = useState<string>(today.toISOString().slice(0, 10));
   const {isLoading, isError, data} = useGetCalendarData({start: from, end: To});
   const queryClient = useQueryClient();
@@ -35,31 +36,40 @@ function Calendar() {
       </HStack>
       {isLoading && <div>Loading...</div>}
       {isError && <div>Error</div>}
-      {data && data.length > 0 ? <ResponsiveCalendar
-      data={data!}
-      from={new Date(from)}
-      to={new Date(To)}
-      emptyColor="#eeeeee"
-      colors={['#61cdbb', '#97e3d5', '#e8c1a0', '#f47560']}
-      margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
-      yearSpacing={40}
-      yearLegendOffset={10}
-      monthBorderColor="#ffffff"
-      dayBorderWidth={2}
-      dayBorderColor="#ffffff"
-      legends={[
-        {
-          anchor: 'bottom-right',
-          direction: 'row',
-          translateY: 36,
-          itemCount: 10,
-          itemWidth: 42,
-          itemHeight: 36,
-          itemsSpacing: 14,
-          itemDirection: 'right-to-left'
-        }
-      ]}
+      <Box w={{base: "xs", lg:"4xl", xl:"4xl", "2xl":"4xl", md:"2xl", sm:"xs"}} h="2xl" bgColor="red">
+      {data ? <ResponsiveCalendar
+        data={data}
+        from={from}
+        to={To}
+        emptyColor="#eeeeee"
+        colors={[ '#61cdbb', '#97e3d5', '#e8c1a0', '#f47560' ]}
+        margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+        yearSpacing={40}
+        monthBorderColor="#ffffff"
+        dayBorderWidth={2}
+        direction={direction}
+        dayBorderColor="#ffffff"
+        tooltip={({day, value, color}: any)=>{
+          return(
+            <Box bgColor={color} p="1" rounded="lg">
+              <Text>à {new Date(day).toDateString()}: {value} Nouvelles reclamations</Text>
+            </Box>
+          )
+        }}
+        legends={[
+            {
+                anchor: 'bottom-right',
+                direction: 'row',
+                translateY: 36,
+                itemCount: 4,
+                itemWidth: 42,
+                itemHeight: 36,
+                itemsSpacing: 14,
+                itemDirection: 'right-to-left'
+            }
+        ]}
     />: <div>No data</div>}
+    </Box>
     </Box>
   )
 }
