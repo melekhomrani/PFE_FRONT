de2757dcd7f1a5bfa221553b07aa0b582ab7f711
yassine <sassou.est100@gmail.com>
2023-05-21 17:31:20 +0100

    fixed spelling mistake

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     <Flex justify="center" alignItems="center" bg="red" py="4" h="10vh" >
       <Flex align="center" justify="center">
         <Text fontSize="md" color="white" textAlign="center">
-          Esprit School - Private High School - Tunisia
+          Esprit School of engineering - Tunisia - 2023
         </Text>
       </Flex>
     </Flex>
